b310891b1a00802d8f97068653887f06190ca7cd
Mahendar <mahendar.ramidi@aurotechcorp.com>
2018-06-08 12:47:41 -0400

    Commit

---
+++ AurotechApp/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  AurotechApp
+//
+//  Created by Innovation Lab on 6/8/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ResultLabel: UILabel!
+    @IBOutlet weak var LabelTwo: UILabel!
+    @IBOutlet weak var LabelOne: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func Add(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func Subtract(_ sender: UIButton) {
+    }
+}
+
